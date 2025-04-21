74f11b06f78ab9745ff7321881456dad5e63c70e
Kristian Rekstad <kre@capraconsulting.no>
2025-04-21 19:43:20 +0200

    Clean up example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import LogTable from "./LogTable";
 import LogDetails from "./LogDetails";
@@ -22,21 +20,8 @@ function App() {
       <main className="flex flex-col items-center justify-center min-h-screen p-4">
         <ModeToggle className="absolute right-4 top-4" />
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Welcome to Tauri + React
+          Log Viewer
         </h1>
-
-        <div className="row">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-          </a>
-          <a href="https://tauri.app" target="_blank">
-            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
         <form
           className="row"
@@ -48,9 +33,14 @@ function App() {
           <Input
             id="greet-input"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                greet();
+              }
+            }}
+            placeholder="Search"
           />
-          <Button type="submit">Greet</Button>
         </form>
         <p>{greetMsg}</p>
 
